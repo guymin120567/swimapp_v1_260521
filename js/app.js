f95aafc73abe36432073e86d938099d0cd9c4bd2
guymin120567 <guymin120567@gmail.com>
2026-05-21 04:52:58 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+
 import { initApp } from "./core/init.js";
+
+/* =========================
+   🧼 SAFARI CLEAN START
+========================= */
+
+(function safariHardReset() {
+
+  // 🔥 service worker 제거 (PWA 겹침 원인 1순위)
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(regs => {
+      regs.forEach(r => r.unregister());
+    });
+  }
+
+  // 🔥 local DOM flush 방지용
+  if (document.documentElement) {
+    document.documentElement.style.display = "none";
+    document.documentElement.offsetHeight;
+    document.documentElement.style.display = "block";
+  }
+
+})();
 
 initApp();
