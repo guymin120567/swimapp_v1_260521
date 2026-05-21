20d1a6249e400eb386b8bfa9568aeb3c32180669
guymin120567 <guymin120567@gmail.com>
2026-05-22 03:55:01 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,47 +2,13 @@ import {
   initController
 } from "./core/controller.js";
 
-// =========================
-// START APP
-// =========================
 window.addEventListener(
   "DOMContentLoaded",
   async ()=>{
 
-    console.log(
-      "DOM LOADED"
-    );
+    const controller =
+      initController();
 
-    try{
-
-      const controller =
-        initController();
-
-      await controller.boot();
-
-      console.log(
-        "APP READY"
-      );
-    }
-    catch(error){
-
-      console.error(
-        "APP START ERROR",
-        error
-      );
-
-      document.body.innerHTML = `
-        <div
-          style="
-            padding:40px;
-            background:#111827;
-            color:white;
-            font-family:sans-serif;
-          "
-        >
-          앱 실행 오류
-        </div>
-      `;
-    }
+    await controller.boot();
   }
 );
