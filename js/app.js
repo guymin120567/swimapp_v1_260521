002f396b2c2a3fb5eace4f6e5076f270d1663268
guymin120567 <guymin120567@gmail.com>
2026-05-22 03:39:42 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,53 +3,46 @@ import {
 } from "./core/controller.js";
 
 // =========================
-// APP START
-// =========================
-async function startApp(){
-
-  try{
-
-    console.log(
-      "APP START"
-    );
-
-    const controller =
-      initController();
-
-    await controller.boot();
-
-    console.log(
-      "APP READY"
-    );
-  }
-  catch(error){
-
-    console.error(
-      "APP BOOT ERROR",
-      error
-    );
-
-    document.body.innerHTML = `
-      <div
-        style="
-          padding:40px;
-          color:white;
-          background:#111827;
-          font-family:sans-serif;
-        "
-      >
-        앱 실행 중 오류 발생
-      </div>
-    `;
-  }
-}
-
-// =========================
-// DOM READY
+// START APP
 // =========================
 window.addEventListener(
   "DOMContentLoaded",
-  ()=>{
-    startApp();
+  async ()=>{
+
+    console.log(
+      "DOM LOADED"
+    );
+
+    try{
+
+      const controller =
+        initController();
+
+      await controller.boot();
+
+      console.log(
+        "APP READY"
+      );
+    }
+    catch(error){
+
+      console.error(
+        "APP START ERROR",
+        error
+      );
+
+      document.body.innerHTML = `
+        <div
+          style="
+            padding:40px;
+            background:#111827;
+            color:white;
+            font-family:sans-serif;
+          "
+        >
+          앱 실행 오류
+        </div>
+      `;
+    }
   }
 );
