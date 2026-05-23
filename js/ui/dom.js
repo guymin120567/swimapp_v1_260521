dde26532da4b93dea240c88662e674680aff073f
guymin120567 <guymin120567@gmail.com>
2026-05-24 05:02:50 +0900

    Update dom.js

--- js/ui/dom.js
+++ js/ui/dom.js
@@ -1,28 +1,102 @@
 export const dom = {
 
+  initialized:false,
+
   app:null,
 
+  // =========================
+  // LIST
+  // =========================
   capList:null,
 
   swimList:null,
 
+  // =========================
+  // TITLE
+  // =========================
   capTitle:null,
 
   swimTitle:null,
 
+  // =========================
+  // RESULT
+  // =========================
   capResultImage:null,
 
   swimResultImage:null,
 
   capResultName:null,
 
-  swimResultName:null
+  swimResultName:null,
+
+  capResultPlaceholder:null,
+
+  swimResultPlaceholder:null
 };
 
+// =========================
+// INIT ROOT
+// =========================
 export function initDOM(){
 
   dom.app =
     document.getElementById(
       "app"
+    );
+}
+
+// =========================
+// CACHE
+// =========================
+export function cacheDOM(){
+
+  dom.capList =
+    document.getElementById(
+      "capList"
+    );
+
+  dom.swimList =
+    document.getElementById(
+      "swimList"
+    );
+
+  dom.capTitle =
+    document.getElementById(
+      "capTitle"
+    );
+
+  dom.swimTitle =
+    document.getElementById(
+      "swimTitle"
+    );
+
+  dom.capResultImage =
+    document.getElementById(
+      "capResultImage"
+    );
+
+  dom.swimResultImage =
+    document.getElementById(
+      "swimResultImage"
+    );
+
+  dom.capResultName =
+    document.getElementById(
+      "capResultName"
+    );
+
+  dom.swimResultName =
+    document.getElementById(
+      "swimResultName"
+    );
+
+  dom.capResultPlaceholder =
+    document.getElementById(
+      "capResultPlaceholder"
+    );
+
+  dom.swimResultPlaceholder =
+    document.getElementById(
+      "swimResultPlaceholder"
     );
 }
