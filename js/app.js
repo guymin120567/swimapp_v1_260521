5d6296001ea71e2c074730830de727868ff2d3b9
guymin120567 <guymin120567@gmail.com>
2026-05-25 04:19:22 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,91 +1,14 @@
 import {
-  initController
-} from "./core/controller.js";
+  renderApp
+} from "./ui/render.js";
 
 // =========================
-// CONTROLLER
+// INIT
 // =========================
-const controller =
-  initController();
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
 
-// =========================
-// START
-// =========================
-async function start(){
-
-  
-  const startedAt =
-    performance.now();
-
-  try{
-
-    // =========================
-    // BOOT
-    // =========================
-    await controller.boot();
-
-    console.log(
-      "BOOT SUCCESS"
-    );
+    renderApp();
   }
-  catch(error){
-
-    console.error(
-      "APP ERROR",
-      error
-    );
-  }
-  finally{
-
-    // =========================
-    // 최소 splash 유지시간
-    // =========================
-    const MIN_SPLASH = 1600;
-
-    const elapsed =
-      performance.now() - startedAt;
-
-    const remain =
-      Math.max(
-        0,
-        MIN_SPLASH - elapsed
-      );
-
-    setTimeout(()=>{
-
-      const splash =
-        document.getElementById(
-          "splash"
-        );
-
-      const app =
-        document.getElementById(
-          "app"
-        );
-
-      requestAnimationFrame(()=>{
-
-        if(app){
-
-          app.style.opacity = 1;
-        }
-
-        if(splash){
-
-          splash.classList.add(
-            "hide"
-          );
-        }
-      });
-
-      setTimeout(()=>{
-
-        splash?.remove();
-
-      },900);
-
-    },remain);
-  }
-}
-
-start();
+);
