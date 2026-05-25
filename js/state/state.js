664b4e5d6bdadba7f2346b5892dde219e56d02a9
guymin120567 <guymin120567@gmail.com>
2026-05-26 05:22:12 +0900

    Update state.js

--- js/state/state.js
+++ js/state/state.js
@@ -1,3 +1,49 @@
+export const defaultState = {
+
+  data: {
+
+    caps: [],
+
+    swimsuits: [],
+
+    records: []
+  },
+
+  selection: {
+
+    capId: null,
+
+    swimId: null
+  },
+
+  ui: {
+
+    activeTab: "roulette",
+
+    activeCapId: null,
+
+    activeSwimId: null,
+
+    isSpinning: false
+  }
+};
+
+let state =
+  structuredClone(
+    defaultState
+  );
+
+// =========================
+// GET
+// =========================
+export function getState(){
+
+  return state;
+}
+
+// =========================
+// SET
+// =========================
 export function setState(
   newState
 ){
@@ -21,4 +67,22 @@ export function setState(
       ...newState.ui
     }
   };
+}
+
+// =========================
+// UI
+// =========================
+export function setActiveTab(tab){
+
+  state.ui.activeTab = tab;
+}
+
+export function setActiveCapId(id){
+
+  state.ui.activeCapId = id;
+}
+
+export function setActiveSwimId(id){
+
+  state.ui.activeSwimId = id;
 }
